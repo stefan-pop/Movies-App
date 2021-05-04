c7a108bee532f2ca090135d4f35ab8f2e6342273
Stefan Pop <stefan9161@gmail.com>
2021-05-04 20:47:03 +0200

    used 'morgan' to log the infos each time a req is initiated

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+app.use(morgan('common'));
+
 app.use(express.static('public'))
 
 app.get('/movies', (req, res) => {
