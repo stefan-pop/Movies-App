0a98a423f9a4391d8d76cbf25d6f213ace294595
Stefan Pop <stefan9161@gmail.com>
2021-05-04 20:33:52 +0200

    created a 'get' route at endpoint '/movies'

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+
+app.get('/movies', (req, res) => {
+    res.json( [
+        {movie_1:'info_1'}, 
+        {movie_2:'info_2'},
+        {movie_3:'info_3'}
+    ])
+})
