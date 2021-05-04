154db8fda5afbe7258787b6cf7cabe431073aa0d
Stefan Pop <stefan9161@gmail.com>
2021-05-04 20:42:48 +0200

    served static files with 'express.static'

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+app.use(express.static('public'))
+
 app.get('/movies', (req, res) => {
     res.json( [
         {movie_1:'info_1'}, 
