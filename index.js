e5592aea7533c1b6912b93e9f28ac44d7c030a6f
Stefan Pop <stefan9161@gmail.com>
2021-05-09 19:40:08 +0200

    deleted the old code

--- index.js
+++ index.js
@@ -4,26 +4,6 @@ const bodyParser = require('body-parser');
 const uuid = require('uuid');
 const app = express();
 
-app.use(morgan('common'));
-
-app.use(express.static('public'))
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Server-side Error');
-})
-
-app.get('/movies', (req, res) => {
-    res.json( [
-        {movie_1:'info_1'}, 
-        {movie_2:'info_2'},
-        {movie_3:'info_3'}
-    ]);
-})
-
-app.get('/', (req, res) => {
-    res.send('Test root path');
-})
 
 app.listen(8080, () => {
     console.log('Server running');
