7d486e06c32bb5cd49e4fead598768f408bd55e3
Stefan Pop <stefan9161@gmail.com>
2021-05-09 19:39:29 +0200

    required body-parser and uuid

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
 const app = express();
 
 app.use(morgan('common'));
