1b62daf4e80cbd6618bb1fcfd37daac3769e1c45
Stefan Pop <stefan9161@gmail.com>
2021-05-16 20:07:40 +0200

    updated the routes to fit the new data format

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/movies/:title', (req, res) => {
 
 // Filter the movies by genre
 app.get('/movies/genre/:genre', (req, res) => {
-    let returnByGenre = movies.filter((x) => {return x.genre == req.params.genre });
+    let returnByGenre = movies.filter((x) => {return x.genre.name == req.params.genre });
     res.json(returnByGenre);
 })
 
@@ -43,10 +43,10 @@ app.get('/movies/details/directors/:director', (req, res) => {
 
 // Create a new user
 app.post('/users', (req, res) => {
-    if(req.body.name && req.body.email) {
+    if(req.body.username && req.body.email) {
         req.body.id = uuid.v4();
         users.push(req.body);
-        res.send(`User ${req.body.name} added successfully.
+        res.send(`User ${req.body.username} added successfully.
         User ID: ${req.body.id}`);
     }else {
         res.status(404).send('Missing name');
@@ -55,18 +55,18 @@ app.post('/users', (req, res) => {
 
 // Update user info
 app.put('/users/:id/:new_username', (req, res) => {
-    let user = users.find((x) => { return x.id == req.params.id});
+    let user = users.find((x) => { return x._id == req.params.id});
     let prevUsername = user.username;
     user.username = req.params.new_username;
      if(user) {
         console.log(user.username);
-        res.send(`The username of ${user.name} was updated from '${prevUsername}' to '${user.username}'`)
+        res.send(`The username was updated from '${prevUsername}' to '${user.username}'`);
      }
 })
 
 // Add a movie as favorite
 app.post('/users/:user/favorites', (req, res) => {
-    let user = users.find((x) => { return x.name == req.params.user});
+    let user = users.find((x) => { return x.username == req.params.user});
 
     if(user) {
         res.send('New movie added to favorites')
@@ -86,10 +86,10 @@ app.delete('/users/:user/favorites/:movie_title', (req, res) => {
 
 // Delete account by the name of the user
 app.delete('/users/:user', (req, res) => {
-    let user = users.find((x) => { return x.name == req.params.user});
+    let user = users.find((x) => { return x.username == req.params.user});
 
     if(user) {
-        res.send(`The account with the name: ${user.name} has been removed successfully`)
+        res.send(`The account with the name: ${user.username} has been removed successfully`)
     }
 })
 
